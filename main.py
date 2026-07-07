69c1147a77d56dd3f504eaa0380d7ffcfed570c7
lange047 <ericeduardolange@gmail.com>
2026-07-07 20:16:51 -0300

    Ajusta inicializacao do prisma e rotas da funcao

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+os.system("prisma generate")
+
+
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
